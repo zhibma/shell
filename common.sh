87bc33c47d90e51f62f5359a86b97ccc8420e1b0
zhibma <40697434+zhibma@users.noreply.github.com>
2020-04-25 16:09:46 +0800

    关于login shell、解释器限定、别名展开

--- common.sh
+++ common.sh
@@ -6,3 +6,19 @@ then
     echo "Permission check failed, must be root." >&2
     exit 1
 fi
+
+# Login shell, whose the interpreter ran with a --login option,
+# which will use ${HOME}/.bashrc to initial the environment.
+
+# limit the interpreter specified by cmdline. As we know,
+# the interpreter is specified by cmdline or script itself.
+INTRPR=$(ps -o comm='' --pid ${$})
+if [ ${INTRPR} != 'sh' -a ${INTRPR} != ${0##*/} ]
+then
+    echo "Error: ${INTRPR} is not the expected interpreter, use sh instead"
+    exit ${VC_FALSE}
+fi
+
+# allow aliase expansion, when the interpreter is bash, disabled by default.
+# However which is enabled when the interpreter is sh.
+shopt -s expand_aliases
