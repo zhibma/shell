7b371684d6627b66f8c68c8be11e7138034bc7fb
zhibma <40697434+zhibma@users.noreply.github.com>
2020-04-25 16:26:14 +0800

    改变工作目录到脚本自身所在路径

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# must be root
+# Must be root
 if [ 0 -ne $(/usr/bin/id -u) ]
 then
     echo "Permission check failed, must be root." >&2
@@ -10,7 +10,7 @@ fi
 # Login shell, whose the interpreter ran with a --login option,
 # which will use ${HOME}/.bashrc to initial the environment.
 
-# limit the interpreter specified by cmdline. As we know,
+# Limit the interpreter specified by cmdline. As we know,
 # the interpreter is specified by cmdline or script itself.
 INTRPR=$(ps -o comm='' --pid ${$})
 if [ ${INTRPR} != 'sh' -a ${INTRPR} != ${0##*/} ]
@@ -19,6 +19,11 @@ then
     exit ${VC_FALSE}
 fi
 
-# allow aliase expansion, when the interpreter is bash, disabled by default.
+# Allow aliase expansion, when the interpreter is bash, disabled by default.
 # However which is enabled when the interpreter is sh.
 shopt -s expand_aliases
+
+# Two method to change work directory to where the script located in, but
+# when script is excuted through system PATH search, both two are invalid.
+cd $(dirname ${0})
+cd ${0%/*} 2>/dev/null
